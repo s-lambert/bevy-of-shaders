23d4411517260fd0611a1debabf3a55297c3cdfc
Scott Lambert <s-lambert@live.com.au>
2023-03-05 15:46:33 +0800

    Add copy of bevy example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::{prelude::*, reflect::TypeUuid, render::render_resource::*};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(MaterialPlugin::<CustomMaterial>::default())
+        .add_startup_system(setup)
+        .add_system(bevy::window::close_on_esc)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    // mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // cube
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        material: materials.add(CustomMaterial {}),
+        // material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
+
+#[derive(AsBindGroup, TypeUuid, Debug, Clone)]
+#[uuid = "a3d71c04-d054-4946-80f8-ba6cfbc90cad"]
+struct CustomMaterial {}
+
+impl Material for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/animate_shader.wgsl".into()
+    }
 }
